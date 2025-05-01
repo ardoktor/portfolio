ba472c5b006755c6ead38a4d4547a125bb52be7c
Arda <arda.asutt@gmail.com>
2025-05-01 13:26:51 +0300

    allowed host problem is fixed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-vq+)5s!^gzipz!erz5(#sc=kw2w$ng#&l-(+!eg^c927w%t8i0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-ru00.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
